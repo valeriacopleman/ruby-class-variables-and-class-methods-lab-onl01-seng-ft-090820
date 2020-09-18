9a1d0e6f07ad32b3727cbbd78bc3ed1f76a7d8df
Valeria  Copleman <valeriacopleman@gmail.com>
2020-09-18 18:50:32 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,35 @@
+class Song 
+  
+  attr_accessor :name, :artist, :genre  
+  
+  @@count = 0 
+  
+  @@genres = []
+  
+  @@artists = []
+  
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1 
+    @@genres << genre 
+    @@artists << artist 
+    
+  end
+  
+  def self.count
+    @@count
+  end
+  
+      
+  
+  
+  def self.genre_count
+  
+    @@genres.each_with_object(@@genre_count={}) do |keys,values| 
+      @@genre_count[keys] = values.count 
+  end
+  return @@genre_count
+end
+end
